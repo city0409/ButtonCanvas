afe86f45c28f62a88f7f97a3b912bbeac5b0b025
city0409 <250924125@qq.com>
2018-02-06 10:21:39 +0800

    Initial commit

---
+++ Assets/MyButton.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class MyButton : MonoBehaviour ,IPointerClickHandler,IPointerEnterHandler
+{
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        print("OnPointerClick");
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        print("OnPointerEnter");
+    }
+}
---
+++ Assets/Test.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test : MonoBehaviour 
+{
+
+	private void Awake() 
+	{
+		
+	}
+	
+	public void OnClick_Test () 
+	{
+        print("OnClick_Test");
+	}
+}
